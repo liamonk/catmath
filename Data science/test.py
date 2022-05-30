10e1027cee3469470ecb14bbccfd36c0c298b9b0
liam.keegan <liam.keegan@iona.school.nz>
2022-05-30 14:28:06 +1200

    small tweaks

---
+++ Data science/test.py
@@ -0,0 +1,11 @@
+import  pandas as pd
+import numpy as pd
+
+# Load in the train and test datasets
+train = pd.read_csv('../input/train.csv')
+test = pd.read_csv('../input/test.csv')
+
+# Store our passenger ID for easy access
+PassengerId = test['PassengerId']
+
+train.head(3)
